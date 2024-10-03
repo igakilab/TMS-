09e548640ec50be0b34f7db64a7c94ef0304a042
ok2580 <gomo20031119@gmail.com>
2024-10-03 11:26:11 +0900

    finish Ground
    マテリアル以外完了
    何らかの当たり判定を受けると前方に床生成・後方の床破壊

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GroundSpawner : MonoBehaviour
 {
     public GameObject GroundTile;
     Vector3 nextSpawnPoint;
 
-    void Spawnfile()
+    public void Spawnfile()
     {
         GameObject temp = Instantiate(GroundTile,nextSpawnPoint,Quaternion.identity);
-        nextSpawnPoint = temp.transform.GetChild(1);
+        nextSpawnPoint = temp.transform.GetChild(1).transform.position;
     }
     void Start()
     {
-        
+        for(int i = 0;i < 10;i++) {
+            Spawnfile();
+        }
     }
 }
